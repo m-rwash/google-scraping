d36f0941f0b196b70655326193871417b9b077f2
Mohamed Rwash <m.rwash93@yahoo.com>
2017-12-29 22:21:17 +0200

    prettying it up with bootstrap

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -2,10 +2,17 @@ class KeywordsController < ApplicationController
   before_action :authorize
 
   def index
-  	@keywords = Keyword.all.order(keyword: :asc)
-  	@keywords = Keyword.search_keyword(params[:search_keyword]) if params[:search_keyword].present?
-  	@keywords = Keyword.search_adwords_urls(params[:search_adwords_urls]) if params[:search_adwords_urls].present?
-  	@keywords = Keyword.search_url(params[:search_url]) if params[:search_url].present?
+  	@keywords = Keyword.all.order(keyword: :asc).page(params[:page])
+                .paginate(:page => params[:page], :per_page => 10)
+
+  	@keywords = Keyword.search_keyword(params[:search_keyword]).page(params[:page])
+                .paginate(:page => params[:page], :per_page => 10) if params[:search_keyword].present?
+
+  	@keywords = Keyword.search_adwords_urls(params[:search_adwords_urls]).page(params[:page])
+                .paginate(:page => params[:page], :per_page => 10) if params[:search_adwords_urls].present?
+
+  	@keywords = Keyword.search_url(params[:search_url]).page(params[:page])
+                .paginate(:page => params[:page], :per_page => 10) if params[:search_url].present?
   	@count = @keywords.count
   end
 
