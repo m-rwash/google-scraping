0a703e9a39ac3f18c17b56ef912487ad363c2b57
Mohamed Rwash <m.rwash93@yahoo.com>
2017-12-29 18:34:24 +0200

    add search abilities to the api

--- app/controllers/api/v1/keywords_controller.rb
+++ app/controllers/api/v1/keywords_controller.rb
@@ -1,38 +1,37 @@
 module Api
   module V1
   	class KeywordsController < ApplicationController
-  	  skip_before_action :verify_authenticity_token
+  	  #skip_before_action :verify_authenticity_token
 	  respond_to :json
-	  #before_action :doorkeeper_authorize!
+	  before_action :doorkeeper_authorize!
 	  def index
 	  	respond_with Keyword.all.order(keyword: :asc)
 	  end
 
 	  def import
-	  	#binding.pry
 	  	file = params.values[0].tempfile
 	  	if file != nil
 	  		Keyword.destroy_all
 	  		Keyword.import(file) 
 	  	end
-	  	# redirect_to keywords_path
+	  	redirect_to api_v1_keywords_path
 	  end
 
 	  def search_keyword
-	  	if params.present? && params[:search_keyword].present?
-	  		respond_with Keyword.search(params[:search_keyword]) 
+	  	if params.present? && params[:term].present?
+	  		respond_with Keyword.search_keyword(params[:term]) 
 	  	end
 	  end
 
 	  def search_adwords_urls
-	  	if params.present? && params[:search_adwords_urls].present?
- 			respond_with Keyword.search_adwords_urls(params[:search_adwords_urls])
+	  	if params.present? && params[:term].present?
+ 			respond_with Keyword.search_adwords_urls(params[:term])
 	  	end
 	  end
 
 	  def search_all_urls
-	  	if params.present? && params[:search_url].present?
-	  		respond_with Keyword.search_url(params[:search_url])
+	  	if params.present? && params[:term].present?
+	  		respond_with Keyword.search_url(params[:term])
 	  	end
 	  end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,28 @@
 Rails.application.routes.draw do
 
-#use_doorkeeper
-namespace :api, defaults: {format: 'json'} do
-  namespace :v1 do
-  	resources :keywords do
-		collection {post :import}
-    collection {get :search}
-	end
-  end
-end	
+  use_doorkeeper
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+    	resources :keywords, only: :index do
+        collection do
+          post :import
+          get :search_keyword
+          get :search_adwords_urls
+          get :search_all_urls
+        end
+  	  end
+    end
+  end	
 
-get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-get '/logout' => 'sessions#destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
+  get '*path', to: 'keywords#index'
   root 'keywords#index'	
-  # get 'keywords/index'
-  # get 'keywords/show'
-  # post 'keywords/import'
 
-  resources :keywords do
+
+  resources :keywords, only: :index do
   	collection {post :import}
   end
 
