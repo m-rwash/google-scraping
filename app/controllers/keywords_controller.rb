6fad90a414d396d2794e61b46f94cea70dab5e9c
Mohamed Rwash <m.rwash93@yahoo.com>
2018-01-04 10:00:06 +0200

    Fixes #10 - Externalize the logic in the index method

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -2,17 +2,12 @@ class KeywordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  	@keywords = Keyword.all.order(keyword: :asc).page(params[:page])
-                .paginate(:page => params[:page], :per_page => 10)
+    @keywords = if is_performing_search?
+                  Keyword.search(search_params).paginate(page: params[:page], per_page: 10) 
+                else 
+                  Keyword.all.order(keyword: :asc).paginate(page: params[:page], per_page: 10)
+                end
 
-  	@keywords = Keyword.search_keyword(params[:search_keyword]).page(params[:page])
-                .paginate(:page => params[:page], :per_page => 10) if params[:search_keyword].present?
-
-  	@keywords = Keyword.search_adwords_urls(params[:search_adwords_urls]).page(params[:page])
-                .paginate(:page => params[:page], :per_page => 10) if params[:search_adwords_urls].present?
-
-  	@keywords = Keyword.search_url(params[:search_url]).page(params[:page])
-                .paginate(:page => params[:page], :per_page => 10) if params[:search_url].present?
   	@count = @keywords.count
 
     respond_to do |format|
@@ -41,6 +36,15 @@ class KeywordsController < ApplicationController
 
   def show
     @keyword = Keyword.find(params[:id])
+  end
+
+  private 
+  def is_performing_search?
+    params.has_key?(:search_keyword) || params.has_key?(:search_adwords_urls) || params.has_key?(:search_url)
+  end
+
+  def search_params
+    params.permit(:search_keyword, :search_adwords_urls, :search_url)
   end
 
 end
